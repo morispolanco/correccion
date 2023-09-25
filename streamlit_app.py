f00600f223fdb4b94ac2af85916fd209689e6601
morispolanco <morispolanco@gmail.com>
2023-09-24 18:21:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,91 +1,53 @@
 import streamlit as st
-import requests
+from googletrans import Translator, LANGUAGES
 from docx import Document
-from io import BytesIO
+from diff_match_patch import diff_match_patch
 
-# Cambiar el título en la pestaña del navegador
-st.set_page_config(page_title="Traductor y Comparador de Texto", layout="centered")
+st.title("Traducción y comparación de documentos")
 
-# Clave API personalizada
-secret_key = "9428f69325adc980cc9b9dc6a0f84a30a3eb86e74787792c581cc44e4c1adfae"
+# Carga del archivo
+uploaded_file = st.file_uploader("Carga un documento Word", type="docx")
+if uploaded_file is not None:
+    doc = Document(uploaded_file)
 
-# Función para traducir texto al inglés
-def translate_to_english(text):
-    url = f"https://ai-translate.pro/api/{secret_key}/es-en"
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "text": text
-    }
+    # Traducción al inglés
+    translator = Translator()
+    translated_texts = []
+    for paragraph in doc.paragraphs:
+        translated_texts.append(translator.translate(paragraph.text, src='es', dest='en').text)
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    result = response.json()
+    # Nuevo documento en inglés
+    doc_en = Document()
+    for text in translated_texts:
+        doc_en.add_paragraph(text)
 
-    return result["result"]
+    # Traducción del inglés al español
+    translated_texts_es = []
+    for paragraph in doc_en.paragraphs:
+        translated_texts_es.append(translator.translate(paragraph.text, src='en', dest='es').text)
 
-# Función para traducir texto al español
-def translate_to_spanish(text):
-    url = f"https://ai-translate.pro/api/{secret_key}/en-es"
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "text": text
-    }
+    # Nuevo documento en español
+    doc_es = Document()
+    for text in translated_texts_es:
+        doc_es.add_paragraph(text)
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    result = response.json()
+    # Comparación
+    diff = diff_match_patch()
+    diff_result = diff.diff_main(doc.text, doc_es.text)
+    diff.diff_cleanupSemantic(diff_result)
+    diff_text = diff.diff_prettyHtml(diff_result)
 
-    return result["result"]
+    # Visualización de la comparación
+    st.write(diff_text, unsafe_allow_html=True)
 
-# Función para comparar textos
-def compare_texts(original_text, translated_text):
-    if original_text == translated_text:
-        return "Los textos son iguales."
-    else:
-        return "Los textos son diferentes."
+    # Descarga del documento comparado
+    def filedownload(doc, name):
+        doc.save(name)
+        st.download_button(
+            label="Descargar documento comparado",
+            data=doc,
+            file_name=name,
+            mime="application/octet-stream"
+        )
 
-# Función para guardar la comparación en un documento Word
-def save_comparison(original_text, translated_text):
-    doc = Document()
-    doc.add_heading("Comparación de Textos", level=1)
-    doc.add_paragraph("Texto original:")
-    doc.add_paragraph(original_text)
-    doc.add_paragraph("Texto traducido:")
-    doc.add_paragraph(translated_text)
-
-    doc.save("comparison.docx")
-
-def main():
-    st.title("Traductor y Comparador de Texto")
-    st.write("Cargue el documento Word en Español:")
-    uploaded_file = st.file_uploader("Seleccione un archivo", type=["docx"])
-
-    if uploaded_file is not None:
-        doc = Document(uploaded_file)
-        original_text = " ".join([p.text for p in doc.paragraphs])
-
-        if st.button("Traducir al Inglés"):
-            translated_text = translate_to_english(original_text)
-            st.write("Texto traducido al Inglés:")
-            st.write(translated_text)
-
-            if st.button("Traducir al Español"):
-                translated_back_text = translate_to_spanish(translated_text)
-                st.write("Texto traducido al Español:")
-                st.write(translated_back_text)
-
-                comparison_result = compare_texts(original_text, translated_back_text)
-                st.write("Comparación de textos:")
-                st.write(comparison_result)
-
-                if st.button("Guardar comparación"):
-                    save_comparison(original_text, translated_back_text)
-                    st.write("Comparación guardada con éxito.")
-
-                    # Proporcionar enlace de descarga para el archivo de comparación
-                    st.markdown("[Descargar comparación](comparison.docx)")
-
-if __name__ == "__main__":
-    main()
+    filedownload(doc_es, "documento_comparado.docx")
