bac4c40ddf28443fd2c21fdeca3738cca42f1ff8
morispolanco <morispolanco@gmail.com>
2023-09-30 02:36:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,25 @@
 import streamlit as st
-import docx2txt
-import docx
-import requests
-import json
 
-# Función para traducir el texto utilizando la API de AI Translate
-def translate_text(text, source_lang, target_lang):
-    url = "https://ai-translate.pro/api/9428f69325adc980cc9b9dc6a0f84a30a3eb86e74787792c581cc44e4c1adfae/{}/{}".format(source_lang, target_lang)
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "text": text
-    }
-    response = requests.post(url, headers=headers, json=data)
-    
-    if response.ok and response.status_code == 200:
-        try:
-            translation = response.json()
-            return translation["text"]
-        except json.decoder.JSONDecodeError:
-            st.error("Error al decodificar la respuesta de la API")
-    else:
-        st.error("Error en la respuesta de la API")
+def main():
+    st.title("Formulario de carga de documentos")
 
-# Configuración de la aplicación de Streamlit
-st.title("Traductor y comparador de documentos")
+    # Mostrar el formulario para ingresar el nombre y la dirección de correo electrónico
+    name = st.text_input("Nombre")
+    email = st.text_input("Correo electrónico")
 
-# Cargar el documento DOCX en español
-uploaded_file = st.file_uploader("Cargar documento DOCX en español", type="docx")
+    # Mostrar el formulario para cargar el documento
+    uploaded_file = st.file_uploader("Cargar documento")
 
-if uploaded_file is not None:
-    # Convertir el documento a texto en español
-    text_es = docx2txt.process(uploaded_file)
-    
-    # Traducir el texto al inglés
-    text_en = translate_text(text_es, "es", "en")
-    
-    # Traducir el texto en inglés de nuevo al español
-    text_es_translated = translate_text(text_en, "en", "es")
-    
-    # Comparar el primer documento en español y el último documento traducido al español
-    comparison = "Los documentos son iguales." if text_es == text_es_translated else "Los documentos son diferentes."
-    
-    # Crear un nuevo documento DOCX con las comparaciones
-    new_doc = docx.Document()
-    new_doc.add_paragraph("Documento original en español:")
-    new_doc.add_paragraph(text_es)
-    new_doc.add_paragraph("Documento traducido al español:")
-    new_doc.add_paragraph(text_es_translated)
-    new_doc.add_paragraph("Comparación:")
-    new_doc.add_paragraph(comparison)
-    
-    # Descargar el nuevo documento DOCX
-    new_doc.save("documento_comparado.docx")
-    st.download_button("Descargar documento con comparaciones", data=None, file_path="documento_comparado.docx")
+    if uploaded_file is not None:
+        # Guardar el archivo cargado en el servidor
+        with open(uploaded_file.name, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        # Mostrar un mensaje de éxito
+        st.success("El documento se ha cargado correctamente.")
+
+        # Mostrar un enlace para descargar el documento
+        st.download_button("Descargar documento", uploaded_file.name, "Documento cargado")
+
+if __name__ == "__main__":
+    main()
